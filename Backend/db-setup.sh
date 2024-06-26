c9249eb0e788d707344b90653abc0e7e1e2e33c7
kieberts <sebastian@kiebert.com>
2024-06-26 11:44:40 +0200

    Remove insert script from db-setup

--- Backend/db-setup.sh
+++ Backend/db-setup.sh
@@ -5,7 +5,9 @@ export DB_HOST=$DB_HOST
 export DB_USER=$DB_USER
 export DB_PASSWORD=$DB_PASSWORD
 
-rm ../uploads/*
+# rm ../uploads/*
+# find ../uploads -type f ! -name 'Hallo.png' ! -name 'Bye.png' -exec rm {} +
+
 
 mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/create_db.sql
-mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/insert_db.sql
+# mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/insert_db.sql
