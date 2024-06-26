9813dc79c50761be16c87a12446b88a2c718953c
kieberts <sebastian@kiebert.com>
2024-06-26 12:14:06 +0200

    Clear db and setup first-set-up

---
+++ Backend/database_script/first_time_insert_db.sql
@@ -0,0 +1,55 @@
+use InventoryProDB;
+
+Insert into
+    roles(id, role_name)
+values
+    (
+        "b01ccc07-693f-42de-b7f5-f2b5307bbcc1",
+        'Admin'
+    );
+
+Insert into
+    user_types(id, type_name)
+values
+    (
+        "7209efef-5a3f-4fab-8a47-7a157c2df829",
+        'Teacher'
+    );
+
+Insert into
+    users(
+        id,
+        first_name,
+        last_name,
+        username,
+        email,
+        phone_number,
+        user_type_id,
+        is_active,
+        registration_accepted
+    )
+values
+    (
+        "d1592a60-1538-4d6a-b3fd-60193622a854",
+        'Admin',
+        'Admin',
+        'Admin',
+        'admin@admin.com',
+        "+0161838382920",
+        "7209efef-5a3f-4fab-8a47-7a157c2df829",
+        0,
+        1
+    );
+
+Insert into
+    user_roles(user_id, role_id)
+values
+    (
+        "d1592a60-1538-4d6a-b3fd-60193622a854",
+        "b01ccc07-693f-42de-b7f5-f2b5307bbcc1"
+    );
+
+Insert into
+    registration_codes(user_id, code)
+values
+    ("d1592a60-1538-4d6a-b3fd-60193622a854", "187");
--- Backend/db-setup.sh
+++ Backend/db-setup.sh
@@ -11,3 +11,4 @@ export DB_PASSWORD=$DB_PASSWORD
 
 mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/create_db.sql
 # mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/insert_db.sql
+mariadb -u $DB_USER --password=$DB_PASSWORD -h $DB_HOST < ./database_script/first_time_insert_db.sql
